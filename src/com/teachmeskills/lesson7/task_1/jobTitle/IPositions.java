786cb4b573f243f71d2a5166acdbab39fa95b5f0
IvanFLINT <rpkdevil@gmail.com>
2024-01-30 05:42:09 +0300

    Lesson_7

---
+++ src/com/teachmeskills/lesson7/task_1/jobTitle/IPositions.java
@@ -0,0 +1,9 @@
+package com.teachmeskills.lesson7.task_1.jobTitle;
+
+/**
+ * This interface contains a method for describing objects
+ */
+public interface IPositions {
+
+    public void description();
+}
